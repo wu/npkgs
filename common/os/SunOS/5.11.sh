4251925cdd293117aba2c4f5172ec084d233afba
Alex White <git@geekfarm.org>
2009-02-16 03:36:00 -0600

    some ugly hacks to work with opensolaris gnome common build env

--- common/os/SunOS/5.11.sh
+++ common/os/SunOS/5.11.sh
@@ -6,12 +6,20 @@ then
   exit 1
 fi
 
+
+# save some env vars
+export HOSTNAME=$HOST
+LESS_PREFIX=$PREFIX
+LESS_NPKG_PATH=$NPKG_PATH
+LESS_NPKG_PREFIX=$NPKG_PREFIX
+
 echo
 echo SOURCING Sun Gnone Desktop Build env.sh
 echo
 . /opt/dtbld/bin/env.sh
 
-
-
-
-
+export HOST=$HOSTNAME
+HOST_SHORT=${HOST%%.*}
+export PREFIX=$LESS_PREFIX
+export NPKG_PREFIX=$LESS_NPKG_PREFIX
+export NPKG_PATH=$LESS_NPKG_PATH
