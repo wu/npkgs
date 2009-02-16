c139e00b1513d24fcee03ad5fb14b0b5b3024dc2
Alex White <git@geekfarm.org>
2009-02-15 20:52:06 -0600

    added build script for emacs

---
+++ editors/emacs/emacs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PKG_NAME="emacs"
+PACKAGE="$PKG_NAME-22.3"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://ftp.gnu.org/pub/gnu/emacs/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
+
