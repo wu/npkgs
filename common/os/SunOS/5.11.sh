1449cbca32b3c791ae447cb8e16104dc387591fd
Alex White <git@geekfarm.org>
2009-02-16 02:52:36 -0600

    adding opensolaris build script to source sun gnome desktop build environment

---
+++ common/os/SunOS/5.11.sh
@@ -0,0 +1,17 @@
+
+
+if [ ! -z "/opt/dtbld/bin/env.sh" ];
+then
+  echo ERROR you must install the sun gnone desktop build first!
+  exit 1
+fi
+
+echo
+echo SOURCING Sun Gnone Desktop Build env.sh
+echo
+. /opt/dtbld/bin/env.sh
+
+
+
+
+
