8b2ec12acef1ae1739502cbd9a1d02bf7aec6966
Alex White <git@geekfarm.org>
2009-02-17 00:39:54 -0600

    use expat from the stack

--- devel/apr-util/apr-util.sh
+++ devel/apr-util/apr-util.sh
@@ -4,15 +4,12 @@ PKG_NAME="apr-util"
 PACKAGE="$PKG_NAME-1.3.4"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://mirror.olnevhost.net/pub/apache/apr/$TARBALL"
-PREREQS="apr-1"
+PREREQS="apr-1 expat"
 
 # source common envs
 . ../../common.sh
 
-CONFIGURE="$CONFIGURE --with-apr=$PREFIX"
-
-# source common envs
-. ../../common.sh
+CONFIGURE="$CONFIGURE --with-apr=$PREFIX/root/apr-1 --with-expat=$PREFIX/root/expat"
 
 common_fetch
 common_prereqs
