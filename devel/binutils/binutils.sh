9257e0de66fac2691f6a08d56c2cb8ab7ade01bc
Alex White <git@geekfarm.org>
2009-02-17 01:33:10 -0600

    adding build script for binutils for gnu ld

---
+++ devel/binutils/binutils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PKG_NAME="binutils"
+PACKAGE="$PKG_NAME-2.19"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://ftp.gnu.org/gnu/binutils/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                           || exit
+
+common_configure
+common_make
+common_install
+common_install_links
+
