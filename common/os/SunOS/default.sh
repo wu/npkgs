3a3484238ae7b32e2a4079791bb7652a94ebbd70
Alex White <git@geekfarm.org>
2009-02-18 17:24:36 -0600

    don't try to send emails about Sun Studio compiler usage!

--- common/os/SunOS/default.sh
+++ common/os/SunOS/default.sh
@@ -1,8 +1,10 @@
-# default path
-
+# default paths
 export CC="cc"
 export CXX="c++"
 
+# don't try to send emails about Sun Studio compiler usage!!!
+export UT_NO_USAGE_TRACKING=1
+export SUNW_NO_UPDATE_NOTIFY=1
 
 
 
