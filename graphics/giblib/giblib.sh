1fae775a5a9c7b7aa5bb3081b1ab8d85634c9f9f
Alex White <git@geekfarm.org>
2009-03-08 18:55:30 -0500

    adding prereq for feh

---
+++ graphics/giblib/giblib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PKG_NAME="giblib"
+PACKAGE="$PKG_NAME-1.2.4"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://linuxbrit.co.uk/downloads/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
+
+
