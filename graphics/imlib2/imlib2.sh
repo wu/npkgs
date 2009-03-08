a5352597161907d4d813aec13794b2985388427f
Alex White <git@geekfarm.org>
2009-03-08 04:19:00 -0500

    compile imlib2 with jpeg support

--- graphics/imlib2/imlib2.sh
+++ graphics/imlib2/imlib2.sh
@@ -9,6 +9,10 @@ PREREQS="freetype1 libpng"
 # source common envs
 . ../../common.sh
 
+CONFIGURE="$CONFIGURE --with-jpeg=yes"
+export LDFLAGS="-L$PREFIX/root/jpeg/lib $LDFLAGS"
+export CPPFLAGS="-I$PREFIX/root/jpeg/include $CPPFLAGS"
+
 common_fetch
 common_prereqs
 common_clean
