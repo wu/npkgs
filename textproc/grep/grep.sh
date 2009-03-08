a0bec85418173d26a8bc23fa3642d5a7725ceb09
Alex White <git@geekfarm.org>
2009-03-08 18:55:59 -0500

    adding a build script for gnu grep

---
+++ textproc/grep/grep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PKG_NAME="grep"
+PACKAGE="$PKG_NAME-2.5.4"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://ftp.gnu.org/pub/gnu/$PKG_NAME/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
