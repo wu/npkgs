d61b47a6621f508d53e3b1babe348f9756d9472f
Alex White <git@geekfarm.org>
2009-03-26 20:57:47 -0500

    gettext - added a patch for acl_* functions on opensolaris

--- devel/gettext/gettext.sh
+++ devel/gettext/gettext.sh
@@ -4,6 +4,7 @@ PKG_NAME="gettext"
 PACKAGE="$PKG_NAME-0.17"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://ftp.gnu.org/pub/gnu/$PKG_NAME/$TARBALL"
+PREREQS="m4"
 
 # source common envs
 . ../../common.sh
@@ -15,6 +16,14 @@ common_untar
 
 # build and install
 cd $PACKAGE                          || exit
+
+if [ "$OS.$OSVER" = "SunOS.5.11" ]
+then
+  echo
+  echo PATCHING for opensolaris acl_* functions
+  echo
+  $PERL -pi -e's|^LDFLAGS = |LDFLAGS = -lsec |g' gettext-tools/*/Makefile* || exit 1
+fi
 
 common_configure
 common_make
