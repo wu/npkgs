08a901c9c2a741883579369e89ae335baf7b5e4a
Alex White <git@geekfarm.org>
2009-03-26 20:24:38 -0500

    added a build script for m4

---
+++ devel/m4/m4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PKG_NAME="m4"
+PACKAGE="$PKG_NAME-1.4.12"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://ftp.gnu.org/gnu/m4/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
