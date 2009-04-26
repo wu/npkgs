ac8fed5a492c132b51f57c9b039be0ffe3a7f2b3
Alex White <git@geekfarm.org>
2009-04-26 14:59:06 -0500

    adding a patch to compile irssi with perl support on OS X

--- irc/irssi/irssi.sh
+++ irc/irssi/irssi.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 PKG_NAME="irssi"
-PACKAGE="$PKG_NAME-0.8.12"
+PACKAGE="$PKG_NAME-0.8.13"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://irssi.org/files/$TARBALL"
-PREREQS="pkg-config glib"
+
+PREREQS="pkg-config glib ncurses"
+
+# todo: can't get this to compile with perl on OS X :/
 
 # source common envs
 . ../../common.sh
 
-CONFIGURE="$CONFIGURE --with-proxy --with-textui"
+#CONFIGURE="$CONFIGURE --enable-shared=no --without-socks --without-bot --with-proxy --with-textui --disable-largefile --disable-ssl --with-perl-lib=site"
+CONFIGURE="$CONFIGURE --with-perl=yes --without-socks --without-bot --with-proxy --with-textui --disable-largefile --disable-ssl --with-perl-lib=site"
 
 # if this is opensolaris, force use of our own glib!
 if [ "$OS.$OSVER" = "SunOS.5.11" ]
@@ -24,6 +28,15 @@ common_untar
 
 # build and install
 cd $PACKAGE                          || exit
+
+if [ "$OS" = "Darwin" ]; then
+    echo
+    echo "Patching for OS X"
+    echo "see: http://bugs.irssi.org/index.php?do=details&task_id=663"
+    echo
+    chmod u+w src/perl/perl-core.c
+    $PATCHCMD src/perl/perl-core.c < ../../files/perl-core.patch || exit
+fi
 
 common_configure
 common_make
