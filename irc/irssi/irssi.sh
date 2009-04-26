8bcfa886d796c0c464fbc89d1b98bd23afe9db7b
Alex White <git@geekfarm.org>
2009-04-26 15:13:57 -0500

    irssi - removing comment about not compiling with perl support on OS X

--- irc/irssi/irssi.sh
+++ irc/irssi/irssi.sh
@@ -7,12 +7,9 @@ URL="http://irssi.org/files/$TARBALL"
 
 PREREQS="pkg-config glib ncurses"
 
-# todo: can't get this to compile with perl on OS X :/
-
 # source common envs
 . ../../common.sh
 
-#CONFIGURE="$CONFIGURE --enable-shared=no --without-socks --without-bot --with-proxy --with-textui --disable-largefile --disable-ssl --with-perl-lib=site"
 CONFIGURE="$CONFIGURE --with-perl=yes --without-socks --without-bot --with-proxy --with-textui --disable-largefile --disable-ssl --with-perl-lib=site"
 
 # if this is opensolaris, force use of our own glib!
