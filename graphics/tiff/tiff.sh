3dfe83848fa1860f9d600513059490bbf8da1661
Alex White <git@geekfarm.org>
2009-05-01 23:40:29 -0500

    adding build script for libtiff

---
+++ graphics/tiff/tiff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PKG_NAME="tiff"
+PACKAGE="$PKG_NAME-3.8.2"
+TARBALL="$PACKAGE.tar.gz"
+URL="ftp://ftp.remotesensing.org/pub/libtiff/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
