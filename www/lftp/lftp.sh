2537bf1cb024d21f80cf0c78049d7d56219cc51c
wu <VVu@geekfarm.org>
2009-09-17 13:17:15 -0500

    upgraded to latest/greatest lftp

--- www/lftp/lftp.sh
+++ www/lftp/lftp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="lftp"
-PACKAGE="$PKG_NAME-3.7.14"
+PACKAGE="$PKG_NAME-4.0.1"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://ftp.yars.free.net/pub/source/lftp/$TARBALL"
 PREREQS="openssl readline"
