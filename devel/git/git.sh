dbf457ca0bfa5e18870d8626d488114c009111b8
wu <VVu@geekfarm.org>
2009-09-16 19:44:40 -0500

    upgrading git to latest/greatest

--- devel/git/git.sh
+++ devel/git/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git"
-PKG_VERSION="1.6.4.3"
+PKG_VERSION="1.6.4.4"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://www.kernel.org/pub/software/scm/git/$TARBALL"
