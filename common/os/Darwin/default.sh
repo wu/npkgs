34772fcf9283e17322254d78950cab9ded311ab1
wu <VVu@geekfarm.org>
2009-09-24 17:45:49 -0500

    adding X11 paths for darwin

--- common/os/Darwin/default.sh
+++ common/os/Darwin/default.sh
@@ -2,6 +2,7 @@
 
 export PATH="$PREFIX/bin:$PREFIX/sbin:/sw/bin:/sw/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin"
 
-
-
-
+export LDFLAGS="-L/usr/X11R6/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/X11R6/include $CPPFLAGS"
+export CFLAGS="-I/usr/X11R6/include $CFLAGS"
+export CXXFLAGS="-I/usr/X11R6/include $CXXFLAGS"
