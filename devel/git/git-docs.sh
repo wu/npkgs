d212077dc6bc792c2ccd08ff034fcc9dd9676360
wu <VVu@geekfarm.org>
2009-10-11 00:12:36 -0500

    upgraded git to 1.6.5

--- devel/git/git-docs.sh
+++ devel/git/git-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git-manpages"
-PKG_VERSION="1.6.3.2"
+PKG_VERSION="1.6.5"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="wget http://www.kernel.org/pub/software/scm/git/$TARBALL"
