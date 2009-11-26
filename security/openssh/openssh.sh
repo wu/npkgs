ede0b1d39c05a2c3ff11267a5a12b39c58b60856
wu <VVu@geekfarm.org>
2009-11-26 06:07:46 -0600

    added openssh 5.3p1

---
+++ security/openssh/openssh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PKG_NAME="openssh"
+PACKAGE="$PKG_NAME-5.3p1"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://mirror.planetunix.net/pub/OpenBSD/OpenSSH/portable/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+CONFIGURE="$CONFIGURE --with-prngd-socket=$PREFIX/var/run/egd-pool --with-privsep-path=$PREFIX/var/empty"
+
+common_fetch
+common_prereqs
+common_clean
+common_untar
+
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install
+common_install_links
