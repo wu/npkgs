d82b005e8fa0f07111beb0b0e185ea127bc45777
wu <VVu@geekfarm.org>
2009-11-26 21:36:31 -0600

    added fetchmail

---
+++ mail/fetchmail/fetchmail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PKG_NAME="fetchmail"
+PACKAGE="$PKG_NAME-6.3.10"
+TARBALL="$PACKAGE.tar.bz2"
+URL="http://download.berlios.de/fetchmail/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+common_fetch
+common_prereqs
+common_clean
+common_untar_bzip
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+
+common_install
+common_install_links
