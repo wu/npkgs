8aa19713a1fd5e0d9c28136adb5555c3edca909a
wu <VVu@geekfarm.org>
2009-11-28 05:09:24 -0600

    add rsync 3.0.6

---
+++ net/rsync/rsync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PKG_NAME="rsync"
+PACKAGE="$PKG_NAME-3.0.6"
+TARBALL="$PACKAGE.tar.gz"
+URL="http://samba.anu.edu.au/ftp/rsync/src/$TARBALL"
+
+# source common envs
+. ../../common.sh
+
+#common_fetch
+common_prereqs
+common_fetch
+common_clean
+common_untar
+
+# build and install
+cd $PACKAGE                          || exit
+
+common_configure
+common_make
+common_install
+common_install_links
