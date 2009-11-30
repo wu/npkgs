047a3326318362433e92ec10f1c07bd97e5297af
wu <VVu@geekfarm.org>
2009-11-30 10:51:48 -0600

    bug fix - creating symlinks for a package that wasn't previously installed

--- common.sh
+++ common.sh
@@ -1,5 +1,10 @@
 # note: this should be sourced from the build scripts!
 
+echo PACKAGE NAME: $PKG_NAME
+echo PACKAGE:      $PACKAGE
+echo TARBALL:      $TARBALL
+echo URL:          $URL
+
 # common env vars for build scripts
 . ../../common/envs.sh
 . ../../common/common_functions.sh
