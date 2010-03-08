abe6ab6d56c123ba0ebe84b4bec5c2415d484635
Alex White <VVu@geekfarm.org>
2010-03-08 11:35:21 -0600

    upgrade openssh to 5.4p1

--- security/openssh/openssh.sh
+++ security/openssh/openssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="openssh"
-PACKAGE="$PKG_NAME-5.3p1"
+PACKAGE="$PKG_NAME-5.4p1"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://mirror.planetunix.net/pub/OpenBSD/OpenSSH/portable/$TARBALL"
 
