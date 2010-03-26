674798581f23dcb7f21c2309af54725d132d3baf
Alex White <VVu@geekfarm.org>
2010-03-26 17:34:53 -0500

    upgraded git to 1.7.0.3

--- devel/git/git.sh
+++ devel/git/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git"
-PKG_VERSION="1.7.0.2"
+PKG_VERSION="1.7.0.3"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://www.kernel.org/pub/software/scm/git/$TARBALL"
