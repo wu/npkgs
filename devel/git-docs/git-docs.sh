9f3a7766c4baafcda57bbe4c6f8d694f9ed80792
Alex White <VVu@geekfarm.org>
2010-04-02 21:47:45 -0500

    upgrade git to 1.7.0.4

--- devel/git-docs/git-docs.sh
+++ devel/git-docs/git-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git-manpages"
-PKG_VERSION="1.7.0.3"
+PKG_VERSION="1.7.0.4"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="wget http://www.kernel.org/pub/software/scm/git/$TARBALL"
