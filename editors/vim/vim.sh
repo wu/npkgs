b50425264b57e06aea5161c77c0d28101db0d343
Alex White <VVu@geekfarm.org>
2010-10-08 21:59:40 -0700

    upgraded vim to 7.3

--- editors/vim/vim.sh
+++ editors/vim/vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="vim"
-PACKAGE="$PKG_NAME-7.2"
+PACKAGE="$PKG_NAME-7.3"
 TARBALL="$PACKAGE.tar.bz2"
 URL="http://ftp.vim.org/pub/vim/unix/$TARBALL"
 
@@ -16,7 +16,7 @@ common_clean
 common_untar_bzip
 
 # build and install
-cd vim72                             || exit
+cd vim73                             || exit
 
 common_configure
 common_make
