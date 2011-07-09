211c210991576ef15435a152ed3d1234e1518fed
Alex White <VVu@geekfarm.org>
2011-07-09 01:02:38 -0700

    upgraded git to 1.7.6

--- devel/git/git.sh
+++ devel/git/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git"
-PKG_VERSION="1.7.3.5"
+PKG_VERSION="1.7.6"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://www.kernel.org/pub/software/scm/git/$TARBALL"
