7749eec5cf87baaa69ae293423eb8f7e969e59e4
Alex White <VVu@geekfarm.org>
2012-07-02 01:45:38 -0700

    upgraded git to 1.7.11.1

--- devel/git/git.sh
+++ devel/git/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git"
-PKG_VERSION="1.7.11"
+PKG_VERSION="1.7.11.1"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://git-core.googlecode.com/files/$TARBALL"
