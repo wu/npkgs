ac4d44c510dd24e0e2d0d9eb19f5f91d26cf7b13
Alex White <VVu@geekfarm.org>
2013-02-04 20:38:04 -0800

    upgrade git to 1.8.1.2

--- devel/git-docs/git-docs.sh
+++ devel/git-docs/git-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_NAME="git-manpages"
-PKG_VERSION="1.8.0"
+PKG_VERSION="1.8.1.2"
 PACKAGE="$PKG_NAME-$PKG_VERSION"
 TARBALL="$PACKAGE.tar.gz"
 URL="http://git-core.googlecode.com/files/$TARBALL"
